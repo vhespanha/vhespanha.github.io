f3861c6dd693f029126af6f643dd97bdd5888294
slvrsrc <126523673+slvrsrc@users.noreply.github.com>
2023-05-12 17:38:03 -0700

    Add files via upload

---
+++ bookmarks.js
@@ -0,0 +1,26 @@
+// Note: having length != 4 will mess with layout based on how the site is styled
+const bookmarks = [
+  {
+    title: "Daily",
+    links: [
+      { name: "AUR", url: "https://aur.archlinux.org/packages" },
+      { name: "Arch Packages", url: "https://archlinux.org/packages/" },
+      { name: "GitHub", url: "https://github.com" },
+    ],
+  },
+  {
+    title: "Media",
+    links: [
+      { name: "Youtube", url: "https://youtube.com" },
+      { name: "Netflix", url: "https://netflix.com" },
+    ],
+  },
+  {
+    title: "Social",
+    links: [
+      { name: "WhatsApp", url: "https://web.whatsapp.com" },
+      { name: "Facebook", url: "https://facebook.com" },
+      { name: "Reddit", url: "https://www.reddit.com" },
+    ],
+  },
+];
